ad3a10d9fb8c39d875b4eaa6635ad9bc50965916
JavierCorralLizarraga <javiercorraljr@hotmail.com>
2022-05-05 13:50:21 -0500

    correccion paso 1

--- parcial/primer_parcial/data/creaMeta.bash
+++ parcial/primer_parcial/data/creaMeta.bash
@@ -1,5 +1,5 @@
 #!/bin/bash
 touch metadata.R
 echo 'autos_colnames <- c(' > metadata.R
-sed -n 61,89p imports-85.names | cut -f 2 -d "." | cut -f 1 -d ":" | sed "4,6d" | sed 's/^.//' | awk '{ printf "'\''%s'\','\n", $0 }' | xargs -n26 -d'\n' >> metadata.R
+sed -n 61,89p imports-85.names | cut -f 2 -d "." | cut -f 1 -d ":" | sed "4,6d" | sed 's/^.//' | awk '{ printf "'\''%s'\','\n", $0 }' | xargs -n26 -d'\n' | sed 's/.$//' >> metadata.R
 echo ')' >> metadata.R
