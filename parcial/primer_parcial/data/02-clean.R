1409b14247c8818ada2bec32c2d05dd1386ab1a4
JavierCorralLizarraga <javiercorraljr@hotmail.com>
2022-05-05 14:13:58 -0500

    clean

--- parcial/primer_parcial/data/02-clean.R
+++ parcial/primer_parcial/data/02-clean.R
@@ -1,14 +1,15 @@
+
 colnames(autos_data) <- autos_clean_colnames(autos_colnames)
 
 problematic_rows <- problems(autos_data)$row
 
 autos_data[problematic_rows,] <- autos_data %>% 
   slice(problematic_rows) %>% 
-  unite(col="all", -seq(1:6), sep = "/", remove=TRUE) %>% 
-  extract(all, into=c("NO3", "NH4", "resto"), regex="([0-9]*.[0-9]{5})([0-9]*.[0-9]*)/(.*)/NA", remove=TRUE) %>% 
+  unite(col="all", -seq(1:26), sep = "/", remove=TRUE) %>% 
+  extract(all, into=c("curb-weight", "engine-type", "resto"), regex="([0-9]+)([a-z]+)/(.*)/NA", remove=TRUE) %>% 
   separate(resto, into=names(autos_data)[9:18], sep="/", remove=TRUE) %>% 
   readr::type_convert() %>% 
-  mutate(NO3=as.character(NO3))
+  mutate(curb-weight=as.character(curb-weight))
 
 autos_data <- autos_data %>% mutate_at(c(2,3), list(autos_clean_data))
 
