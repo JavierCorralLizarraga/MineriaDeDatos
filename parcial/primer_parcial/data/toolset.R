fb9b2533b1774813b005c071f405e0dc2e18b003
JavierCorralLizarraga <javiercorraljr@hotmail.com>
2022-05-05 13:55:04 -0500

    imputaciones a utils

--- parcial/primer_parcial/data/toolset.R
+++ parcial/primer_parcial/data/toolset.R
@@ -1,29 +1,3 @@
-load <- function(){
-  if(!file.exists('autos.rds')){
-    autos_loc <- 'imports-85.csv'
-    
-    autos_data <- read_csv(algas_url, 
-                      col_names = autos_colnames,
-                      na = 'XXXXXXX')
-    saveRDS(autos_data, "autos.rds")
-    print('autos.rds se bajó guardó\n')
-  }
-  else{
-    warning('autos.rds ya existe\n')
-    autos_data <- readRDS("autos.rds")
-  }
-  
-  return(autos_data)
-}
-
-autos_clean_colnames <- function(x){
-  str_replace_all(tolower(x),"/| ",'_')
-}
-
-autos_clean_data <- function(x){
-  str_replace_all(tolower(x),"_",'')
-}
-
 tipos_de_graficas <- function(datos, todas=TRUE, lista){
   if(todas == TRUE && missing(lista)) {
     ggpairs(datos)
