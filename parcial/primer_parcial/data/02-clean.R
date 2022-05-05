f3e78b4491912f97fdf3dd9fa44d5780dd39c1d8
ElDamo <36738020+ElDamo@users.noreply.github.com>
2022-05-05 15:42:16 -0500

    Add files via upload

--- parcial/primer_parcial/data/02-clean.R
+++ parcial/primer_parcial/data/02-clean.R
@@ -1,16 +1,13 @@
-
 colnames(autos_data) <- autos_clean_colnames(autos_colnames)
 
 problematic_rows <- problems(autos_data)$row
 
 autos_data[problematic_rows,] <- autos_data %>% 
   slice(problematic_rows) %>% 
-  unite(col="all", -seq(1:26), sep = "/", remove=TRUE) %>% 
-  extract(all, into=c("curb-weight", "engine-type", "resto"), regex="([0-9]+)([a-z]+)/(.*)/NA", remove=TRUE) %>% 
-  separate(resto, into=names(autos_data)[9:18], sep="/", remove=TRUE) %>% 
-  readr::type_convert() %>% 
-  mutate(curb-weight=as.character(curb-weight))
+  unite(col="all", -seq(1:13), sep = "/", remove=TRUE) %>% 
+  extract(all, into=c("curb-weight", "engine-type", "resto"), regex="(^[0-9]{4})([a-z]{3,5})/(.*)/NA", remove=TRUE) %>%
+  separate(resto, into=names(autos_data)[16:26], sep="/", remove=TRUE)%>%
+  mutate(across(.cols=19:26, .fns=as.numeric))
 
-autos_data <- autos_data %>% mutate_at(c(2,3), list(autos_clean_data))
 
 autos_data <- readr::type_convert(autos_data)
---
+++ parcial/primer_parcial/data/autos.R
@@ -0,0 +1,44 @@
+library(readr)
+library(stringr)
+library(tidyr)
+library(dplyr)
+library(ggplot2)
+library(ggthemes)
+
+source("metadata.R"  , encoding = 'UTF-8')
+source("utils.R"     , encoding = 'UTF-8')
+source("00-load.R"   , encoding = 'UTF-8')
+source("01-prepare.R", encoding = 'UTF-8')
+source("02-clean.R",   encoding = 'UTF-8')
+
+summary(autos_data)
+
+autos_data<-autos_data %>% mutate_at(c(3,4,5,6,7,8,9,10,15,16,18),list(as.factor))
+
+glimpse(autos_data)
+
+graficar_por_tipo <- function(var1, var2){
+  if (is.factor(var1) && is.factor(var2)){
+    plot(var1, var2)
+  }else if(is.numeric(var1) && is.numeric(var2)){
+    plot(var1, var2)
+  } else { # numeric y factor
+    plot(var1, var2)
+  }
+}
+
+tipos_de_graficas <- function(datos, todas=FALSE, lista){
+  if(todas == TRUE && missing(lista)) {
+    combn(datos, 2, FUN = graficar_por_tipo)
+  }else{
+    comb(datos[lista], 2, FUN = graficar_por_tipo)
+  }
+}
+
+tipos_de_graficas(autos_data,todas = TRUE)
+
+
+for(col in colnames(autos_data)){
+  ggplot(autos_data,aes(col))+
+    geom_histogram()
+}
