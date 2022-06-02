473a29a727cc35d1fb2a9fcd6f00f2cf20984c4b
Javier Corral Lizárraga <javiercorraljr@hotmail.com>
2022-06-02 17:50:13 -0500

    feather

--- Final/spaceship-titanic/main.R
+++ Final/spaceship-titanic/main.R
@@ -5,10 +5,17 @@ library(dplyr)
 library(ggplot2)
 library(ggthemes)
 library(VIM)
+library(feather)
+
+titanic_train = loadTrain()
 
 source("utils.r", encoding = "UTF-8")
 source("clean.r", encoding = "UTF-8")
 
-titanic_train = loadTrain()
 
 titanic_train <- (kNN(titanic_train, k = 10))[1:19]
+
+path <- "titanic_train.feather"
+df <- titanic_train
+write_feather(df, path)
+
