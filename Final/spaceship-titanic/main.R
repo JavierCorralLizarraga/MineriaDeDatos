b3ded66849694288e4d982f1b61cfa0bc40b5a3a
RicardoMeadowcroft <39323760+RicardoMeadowcroft@users.noreply.github.com>
2022-06-03 15:38:44 -0500

    Update main.R

--- Final/spaceship-titanic/main.R
+++ Final/spaceship-titanic/main.R
@@ -7,15 +7,23 @@ library(ggthemes)
 library(VIM)
 library(feather)
 
-titanic_train = loadTrain()
-
 source("utils.r", encoding = "UTF-8")
 source("clean.r", encoding = "UTF-8")
 
+titanic_train <- loadTrain()
+
+titanic_train <- clean(titanic_train)
 
 titanic_train <- (kNN(titanic_train, k = 10))[1:19]
 
 path <- "titanic_train.feather"
-df <- titanic_train
-write_feather(df, path)
+write_feather(titanic_train, path)
 
+titanic_test <- loadTest()
+
+titanic_test <- clean(titanic_test)
+
+titanic_test <- (kNN(titanic_test, k = 10))[1:19]
+
+path <- "titanic_test.feather"
+write_feather(titanic_test, path)
