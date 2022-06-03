9c425d02f7e6c373d79741542ee7033a991f63be
RicardoMeadowcroft <39323760+RicardoMeadowcroft@users.noreply.github.com>
2022-06-03 15:33:54 -0500

    Update clean.r

--- Final/spaceship-titanic/clean.r
+++ Final/spaceship-titanic/clean.r
@@ -1,19 +1,23 @@
-titanic_train <- extract(titanic_train, 1, into=c("grupo", "id"), regex="([0-9]{4})_([0-9]{2})", remove=TRUE) 
-
-titanic_train <- extract(titanic_train, "Cabin", into=c("deck", "room_num", "side"), regex="(.)/(.)/(.)", remove=TRUE) 
-
-titanic_train <- extract(titanic_train, "Name", into=c("fname", "lname"), regex="(.*) (.*)", remove=TRUE) 
-
-titanic_train <- titanic_train %>% mutate(grupo=as.numeric(grupo)) %>% 
-  mutate(id=as.numeric(id)) %>%
-  mutate(room_num=as.numeric(room_num)) %>%
-  mutate(HomePlanet=as.factor(HomePlanet)) %>%
-  mutate(Destination=as.factor(Destination)) %>%
-  mutate(deck=as.factor(deck)) %>%
-  mutate(side=as.factor(side)) %>%
-  mutate(fname=as.factor(fname)) %>%
-  mutate(lname=as.factor(lname))
-
-glimpse(titanic_train)
-
-titanic_train <- titanic_train %>% mutate(gastos = RoomService + FoodCourt + ShoppingMall + Spa + VRDeck)
+clean <- function(titanic){
+  titanic <- extract(titanic, 1, into=c("grupo", "id"), regex="([0-9]{4})_([0-9]{2})", remove=TRUE) 
+  
+  titanic <- extract(titanic, "Cabin", into=c("deck", "room_num", "side"), regex="(.)/(.)/(.)", remove=TRUE) 
+  
+  titanic <- extract(titanic, "Name", into=c("fname", "lname"), regex="(.*) (.*)", remove=TRUE) 
+  
+  titanic <- titanic %>% mutate(grupo=as.numeric(grupo)) %>% 
+    mutate(id=as.numeric(id)) %>%
+    mutate(room_num=as.numeric(room_num)) %>%
+    mutate(HomePlanet=as.factor(HomePlanet)) %>%
+    mutate(Destination=as.factor(Destination)) %>%
+    mutate(deck=as.factor(deck)) %>%
+    mutate(side=as.factor(side)) %>%
+    mutate(fname=as.factor(fname)) %>%
+    mutate(lname=as.factor(lname))
+  
+  glimpse(titanic)
+  
+  titanic <- titanic %>% mutate(gastos = RoomService + FoodCourt + ShoppingMall + Spa + VRDeck)
+  
+  return titanic
+}
